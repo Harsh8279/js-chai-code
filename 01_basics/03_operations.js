131de316facac40fb669bac40e0f11696a3500cb
Harsh Patel <65580408+Harsh8279@users.noreply.github.com>
2024-03-31 19:27:24 +0000

    Operations part completed

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,103 @@
+console.log("2+2 -->",2+2);
+console.log("2*2 -->",2*2);
+console.log("2**3 -->",2**3);
+console.log("2/3 -->",2/3);
+console.log("2%3 -->",2%3);
+
+
+let greetings = "Hello, "
+let name_of_client = "Harsh!!"
+
+console.log("greetings+name_of_client --> ",greetings+name_of_client)
+let sum_of_string_and_num = "1"+2
+console.log(sum_of_string_and_num)
+
+sum_of_string_and_num = 1+"2"
+console.log(sum_of_string_and_num)
+
+sum_of_string_and_num="1"+2+2
+console.log(sum_of_string_and_num)
+
+
+sum_of_string_and_num=1+2+"2"
+console.log(sum_of_string_and_num)
+
+boolean_value = true
+console.log("boolean_value --> ",boolean_value);
+console.log("+boolean_value --> ",+boolean_value);
+console.log("5+boolean_value --> ",5+boolean_value);
+
+boolean_value = false
+console.log("boolean_value --> ",boolean_value);
+console.log("+boolean_value --> ",+boolean_value);
+console.log("5+boolean_value --> ",5+boolean_value);
+
+null_value = null
+console.log("null_value --> ",null_value);
+console.log("+null_value --> ",+null_value);
+
+
+undefined_value = undefined
+console.log("undefined_value --> ",undefined_value);
+console.log("+undefined_value --> ",+undefined_value);
+
+
+
+empty_value = ""
+console.log("empty_value --> ",empty_value);
+console.log("+empty_value --> ",+empty_value);
+
+one_space_value = " "
+console.log("one_space_value --> ",one_space_value);
+console.log("+one_space_value --> ",+one_space_value);
+
+
+two_space_value = "  "
+console.log("two_space_value --> ",two_space_value);
+console.log("+two_space_value --> ",+two_space_value);
+
+
+let number_value = 5
+
+console.log("number_value -->",number_value)
+console.log("number_value++ -->",number_value++)
+console.log("number_value -->",number_value)
+console.log("++number_value -->",++number_value)
+
+
+/*
+
+Output :-
+
+2+2 --> 4
+2*2 --> 4
+2**3 --> 8
+2/3 --> 0.6666666666666666
+2%3 --> 2
+greetings+name_of_client -->  Hello, Harsh!!
+12
+12
+122
+32
+boolean_value -->  true
++boolean_value -->  1
+5+boolean_value -->  6
+boolean_value -->  false
++boolean_value -->  0
+5+boolean_value -->  5
+null_value -->  null
++null_value -->  0
+undefined_value -->  undefined
++undefined_value -->  NaN
+empty_value -->  
++empty_value -->  0
+one_space_value -->   
++one_space_value -->  0
+two_space_value -->    
++two_space_value -->  0
+number_value --> 5
+number_value++ --> 5
+number_value --> 6
+++number_value --> 7
+
+*/
